a05f9ce8491326d4083ab4bfbb42d9c27551f216
Pankaj Labade <pankajlabade99@gmail.com>
2023-07-17 01:48:18 +0200

    Create WorldCupScoreboardTest.java

---
+++ WorldCupScoreboardTest.java
@@ -0,0 +1,18 @@
+package com.game.football.scoreboard.scoreboard;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class WorldCupScoreboardTest {
+    private Scoreboard scoreboard;
+
+    @BeforeEach
+    public void setUp() {
+        scoreboard = new WorldCupScoreboard();
+    }
+
+    @Test
+    public void test_startMatch() {
+      
+    }
+  }
