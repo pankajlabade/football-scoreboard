56def8edff4ff3172d5550132faa407106b88553
pankaj <pankajlabade99@gmail.com>
2023-07-17 02:41:23 +0200

    Created interface Scoreboard.java

---
+++ Scoreboard.java
@@ -0,0 +1,8 @@
+package com.game.football.scoreboard;
+public interface Scoreboard {
+    void startMatch(String homeTeam, String awayTeam);
+
+    void updateScore(String homeTeam, String awayTeam, int homeScore, int awayScore);
+
+    void finishMatch(String homeTeam, String awayTeam);
+}
